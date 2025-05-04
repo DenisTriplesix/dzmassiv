4eb9c1c3b2e4ff806015851d5315185bb57f65ef
denis <denisbelyavski2001@gmail.com>
2025-05-04 16:39:07 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int array [] = {45,78,-15, 12, 34, 6, 90, 7, -2, -45};
+        int maxNumber = array [6] ; // беру из массива максимальное число
+        System.out.println("Max number"+ maxNumber);
+        int minNumber = array [9]; // беру из массива минимальное число
+        System.out.println("Min number"+ minNumber);
+    }
+}
